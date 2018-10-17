5cf68ea4186ed0831ebe1a6f4cd7c138ffef5515
Rohan Yadav <rohan.yadav@gmail.com>
2018-10-17 05:51:32 -0700

    Python operators & variables

---
+++ Day1-AboutPython/Py2OperatorsAndVariables.py
@@ -0,0 +1,24 @@
+#Few python operators are - +, -, *, /, **(Exponent), //(integer division), %(modulus)
+
+#Additon(+), Subtraction(-), Multiplication(*), Division(/)
+print(4+5)
+print(8-3)
+print(5+5-20)
+print(9*9)
+
+#python division / operator returns float value(decimal), even if both operands are of integer type
+print(8/2)
+print(8/2.0)
+print(5/3)
+
+#to get integer value from division use // integer division operator
+print(8//2)
+print(5//3)
+
+#but if anyone one operand is of decimal (float) type, result will always be in decimal(float)
+print(8//2.0)
+print(8.0//2)
+
+#operators precedence from high to low (left to right)
+#Exponent or power(**) --> Modulus(%) --> Integer division(//) --> Division(/) --> Multiplication(*) --> Subtraction(-) --> Additon(+)
+#values inside () paranthesis always gets first preference
