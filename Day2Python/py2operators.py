f365e855dd1dfeafffcd54d6979d57f3cefa0573
Rohan Yadav <30825787+vrrohan@users.noreply.github.com>
2018-10-17 04:37:11 -0700

    Delete py2operators.py

--- Day2Python/py2operators.py
+++
@@ -1,9 +0,0 @@
-print(2+3)
-print(3/2)
-print(3//2)
-print(4*5)
-print(3//1.0)
-#you cannot add string & integer in python
-#print("hello" + 3) --> this line will give error
-#to resolve this, use str() , that converts any object to string object
-print("hello" + str(3))
