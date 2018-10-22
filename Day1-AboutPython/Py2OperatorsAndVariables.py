913197b8213b1ca15392e5f5f162067fd87aa8c3
Rohan Yadav <rohan.yadav@gmail.com>
2018-10-22 05:20:42 -0700

    more about python types & print() statement

--- Day1-AboutPython/Py2OperatorsAndVariables.py
+++ Day1-AboutPython/Py2OperatorsAndVariables.py
@@ -1,24 +1,79 @@
 #Few python operators are - +, -, *, /, **(Exponent), //(integer division), %(modulus)
 
 #Additon(+), Subtraction(-), Multiplication(*), Division(/)
-print(4+5)
-print(8-3)
-print(5+5-20)
-print(9*9)
+print(4+5)      #prints 9
+print(8-3)      #prints 5
+print(5+5-20)   #prints -10
+print(9*9)      #prints 81
 
 #python division / operator returns float value(decimal), even if both operands are of integer type
-print(8/2)
-print(8/2.0)
-print(5/3)
+print(8/2)      #prints 4.0
+print(8/2.0)    #prints 4.0
+print(5/3)      #prints 1.666666666666667
 
 #to get integer value from division use // integer division operator
-print(8//2)
-print(5//3)
+print(8//2)     #prints 4
+print(5//3)     #prints 1
 
 #but if anyone one operand is of decimal (float) type, result will always be in decimal(float)
+#both prints 4.0
 print(8//2.0)
 print(8.0//2)
 
 #operators precedence from high to low (left to right)
 #Exponent or power(**) --> Modulus(%) --> Integer division(//) --> Division(/) --> Multiplication(*) --> Subtraction(-) --> Additon(+)
 #values inside () paranthesis always gets first preference
+
+#** is Exponent operator i.e. x**y is y to power of x
+print(3**3)     #prints 27
+print(9**3)     #prints 729
+
+#% is modulus operator
+print(8%3)              #prints 2
+print(23%7)             #prints 8
+print(2+2**2%3)         #prints 3
+
+#3 main python data types are - Integers, Floating point numbers, Strings
+#Example of integers are : -2, -1, 0, 5, 9
+#Example of floating numbers are : -0.5, -22.57, 0.0, 2.37
+#Example of string are : 'a', "hello", 'welcome to python', "11th sept 2001"
+#Anything within single or double quotes is a string
+#'' is an empty string
+quotes = ''
+print("quote is : " + quotes)
+quotes = "beautiful better than ugly..."
+print("now quote is : " + quotes)
+
+#to define variable in python, just name it with equals(=) sign & assign some value to it
+#no need to define their data types
+numA = 10
+numB = 20
+print("sum is : " , (numA+numB))    #prints 30
+
+numC = numA/numB
+print("division is : " , numC)      #prints 0.5
+
+#Some rules regarding variable names in python are -
+#1. Better practice to use only one world
+#2. Can use letters, numbers & underscore(_)
+#3. cannot start variable name with a number(or digit)
+#4. Avoid using python reserved words as variable names
+#5. Good practice to name variables in lowercase
+#Some valid variable names are - bal, current_bal, _spam, account22
+#Some invalid variable names are - current-bal(hyphens not allowed), current balance(spaces not allowed), 42people(cannot start with a number), total_sum$($ special character not allowed)
+#Python variable names are case sensitive, so - spam, SPAM, sPaM, Spam, SPAm all are different
+spam = "this is a spam mail"
+SPAM = "this is BOLD spam mails"
+print(spam + "\n" + SPAM)
+
+#\n is a new line character - anything after \n will be displayed in a new line
+print("hello is the line 1\nThis line will be next line\nThis line will be 3rd line")
+
+#\t to add tab(4 or more spaces before) to your text, below line prints
+#OOP languages are :
+#	1. C++
+#	2. Java
+#	3. Python
+#	4. C Sharp
+#\n\t means - move to a new line & start next line with a tab
+print("OOP languages are : \n\t1. C++\n\t2. Java\n\t3. Python\n\t4. C Sharp")
---
+++ Day1-AboutPython/Py3MoreAboutPrintAndStrings.py
@@ -0,0 +1,88 @@
+#+ is used to concatenate 2 or more Strings
+print("Alice " + "Bob")
+name = "Rohan"
+state = "Delhi"
+country = "India"
+print("Username is : " + name + "\nCurrently living in : " + state + ", " + country)
+
+#You cannot add string & a integer, it will give - TypeError : must be str, not int
+#info = "Your age is " + 22;     #TypeError
+#print(info)
+
+#when using both inside print() either use , or convert int value to string values
+print("Your age is : ", 22)
+
+#To convert int value to string use - str()
+age = 17
+print("Your age is : " + str(age))
+
+#To take input from user , use - input()
+name = input("Please enter your name : ")
+print("Nice to meet you : " + name)
+
+#But input() returns value in form of string
+age = input("Enter you age : ")
+#age is of string type else, it will give TypeError on using with +
+print("Your age is : " + age)
+
+#To convert string to integer, use - int()
+ageAfter5Years = int(age) + 5
+#Since it is a integer now, we need to convert it back to string
+print("Your age after 5 years will be : " + str(ageAfter5Years))
+
+#With python, you can use + operator only to add two integers or floats or concatenate 2 Strings
+#You cannot add (String+integer) in python, unlike in java.
+#For string+integer, you have to use str() or int() methods
+print(int("5"))
+
+#If you pass string value to int() which cannot be converted back to integer, it will give ValueError : invalid interal for int
+#i.e. "hello" cannot be converted back to some integer value
+#print(int("hello")) will give TypeError
+
+#Similarly float() - it will return float form of the values you passed
+print(float(99))
+print(float(-55))
+print(float(0))
+
+#passing value that cannot be converted back to float will give - ValueError, "python" cannot be converted into float
+#print(float("python"))
+
+#type() returns type of literal(or value) that is passed. type() returns bool i.e. either true or false (not string type)
+print(type(5))                  #prints <int>
+print(type(99.55))              #prints <float>
+print(type('hello python'))     #prints <str>
+print(type(True))               #prints <bool>
+
+#so before converting illegal types for TypeError, always check values that are passed using - is
+#since type() returns type of value that was passed, using type() + is = returns bool , which is again not of type str
+#hence to concatenate both , convert bool value back to str type
+print("Is 5 an integer : " + str((type(5) is int)))
+print("Is 99.55 a float : " + str((type(99.55) is float)))
+print("Is 'hello python' a string object : " + str((type("hello python") is str)))
+
+#Hence 4 methods we see - str(), int(), float(), type()
+#1. str() converts passed value back to string type
+#2. int() converts passed value back to integer type. Gives TypeError for string values
+#3. float() converts passed value back to float type. Gives TypeError for string values
+#4. type() returns type of value that was passed
+print(str(29))
+print(str(-99) + " type is " + str(type(str(-99))))
+print(int(3.14))
+print(int(-0.99))
+print(float(10))
+print(str(-3.14) + " type is " + str(type(str(-3.14))))
+
+#There is another type in python named as bool having only 2 values, either - True or false
+#For integers- 0 is False, else everything is True
+print(bool(-33))        #True
+print(bool(1))          #True
+print(bool(0))          #False
+
+#For string - "" empty string is False, else everything is true
+print(bool("hello"))    #True
+print(bool(""))         #False
+
+#For floats - 0.0 is False, else everything is True
+print(bool(-3.14))      #True
+print(bool(4.55))       #True
+print(bool(0.0))        #False
