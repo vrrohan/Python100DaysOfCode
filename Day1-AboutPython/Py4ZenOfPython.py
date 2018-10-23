5bf4605411bf6b82a90f809cce1bf33da39ac525
Rohan Yadav <rohan.yadav@gmail.com>
2018-10-23 05:36:13 -0700

    python zen of code-19 set of rules for writing good code

---
+++ Day1-AboutPython/Py4ZenOfPython.py
@@ -0,0 +1,7 @@
+#Zen of Python is Python community 20 set of Philosphy
+#This set contains brief set of principles for writing good python code & what you should need to do to write more pythonic code
+#Some of them are - 1. Beautiful is better than Ugly, 2. Readability counts, 3. Simple is better than complex
+#So in order to write a good pythonic code, you code must look beautiful, looks readable so that anyone reading your code can understand easily what logic you're trying to build etc
+#They are known as PEP20- The Zen of Python, there are actually 20 of them, but only 19 of them is written down
+#To see all 19 of them , you only need to import module named - 'this'
+import this  
