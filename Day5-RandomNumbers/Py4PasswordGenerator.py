ab3b7529ea8d19cbc3f6402b64b58b8792985969
Rohan Yadav <rohan.yadav@gmail.com>
2018-12-14 16:26:34 +0530

    Python code to generate random strong password taking user infomation

--- Day5-RandomNumbers/Py4PasswordGenerator.py
+++ Day5-RandomNumbers/Py4PasswordGenerator.py
@@ -2,7 +2,6 @@
 #Username length : minimum 5, maximum 20
 #birth year : 1950 - 2018
 #birth month : only first 3 characters of month like - Jan, Feb, Sep etc
-#phrase : minimum length 5, maximum length 30
 #mobile number : length exactly 10 digits & cannot start with 0
 import random
 import string
@@ -13,52 +12,110 @@ day = str(input("Day : "))
 month = str(input("Month : "))
 year = str(input("Year : "))
 mobNumber = str(input("Enter mobile number : "))
-#phrase = input("Enter your catch phrase : ")
 
-listOfPossibleUserNames = []
+#String which stores our generated password , which is strong & minimum length of 10
+finalStrongPassword = ''
+#Strong password contains combinations of - uppercase, lowercase, digits & special Characters
+#So we create list of all these to store our all combinations of user information
+listOfLowerCaseUserNames = []
+listOfLowerCaseUserNameInitials = []
+listOfUpperCaseUserNames = []
+listOfUpperCaseUserNameInitials = []
+listOfPossibleMobileNumbers = []
+listOfPossibleDigits = []
+listOfPossibleSpecialCharacters = []
 
-
-if ((len(username)<5) or (len(username)>20)) :
+#Here check whether length of username is less than 5 or not
+if ((len(username)<5) or (len(username)>30)) :
     print("Too small username")
 else :
-    #create list of random group of characters from username
+    #Splittinh user's firstname & lastname into a list separated by spaces
     listUserName = username.split(' ')
-    trimmedList = ''.join(listUserName)
-    for i in range(len(trimmedList)-3) :
-        listOfPossibleUserNames.append(trimmedList[i:i+4])
+    #We trimmed & remove spaces between user's firstname & lastname
+    trimmedUserName = ''.join(listUserName)
+    #get both uppercase & lowercase form of trimmed username
+    trimmedUpperUserName = trimmedUserName.upper()
+    trimmedLowerUserName = trimmedUserName.lower()
+    #Creating & appending all different (group of 4) possible username's into a list
+    #Example- If name is 'wayne rooney' , possible group of 4 characters combination is : ['wayn', 'ayne', 'yner', 'nero', 'eroo', 'roon', 'oone', 'oney']
+    for i in range(len(trimmedUserName)-3) :
+        listOfLowerCaseUserNames.append(trimmedUserName[i:i+4])
+    #Creating possible use cases from initials of username
+    #If name is - 'Wayne Rooney' - initials created were ['wa', 'ne', 'ro', 'ey', 'wr', 'waro']
+    #All this possible combinations were stored in another list of initials
+    for i in range(2) :
+        listOfLowerCaseUserNameInitials.append(listUserName[i][:2])
+        listOfLowerCaseUserNameInitials.append(listUserName[i][len(listUserName[i])-2:])
+    listOfLowerCaseUserNameInitials.append(listUserName[0][:2] + listUserName[1][:2])
+    listOfLowerCaseUserNameInitials.append(listUserName[0][0] + listUserName[1][0])
 
+#We check if lowercase username list is filled, it means we have all possible combinations of username saved
+#We then save all these combinations into another parallel list of uppercase combinations
+#Example:- lowercase group of 4 username :- 'wayn' --> 'WAYN' is saved into listOfUpperCaseUserNames
+#Similarly , each lowercase combination is converted into uppercase combination & stored in different list
+if(len(listOfLowerCaseUserNames)>0) :
+    for i in range(len(listOfLowerCaseUserNames)) :
+        listOfUpperCaseUserNames.append(listOfLowerCaseUserNames[i].upper())
 
+#Example -initials in lowercase : ['wa', 'ne', 'ro', 'ey', 'wr', 'waro'] is parallely saved into listOfUpperCaseUserNameInitials[]
+#initials in uppercase : ['WA', 'NE', 'RO', 'EY', 'WR', 'WARO']
+if(len(listOfLowerCaseUserNameInitials)>0) :
+    for i in range(len(listOfLowerCaseUserNameInitials)) :
+        listOfUpperCaseUserNameInitials.append(listOfLowerCaseUserNameInitials[i].upper())
+
+#Checking whether mobile number is exactly of length = 10 & didn't start with 0
 if ((mobNumber.startswith('0')) or (len(mobNumber)!=10)) :
     print('Invalid mobile number')
 else :
-    listOfPossibleMobileNumbers = [mobNumber[0:5], mobNumber[len(mobNumber)-3:]]
+    #If not, append first 5 & last 3 digits of mobile number to list of possible mobile numbers
+    listOfPossibleMobileNumbers.append(mobNumber[0:5])
+    listOfPossibleMobileNumbers.append(mobNumber[len(mobNumber)-3:])
 
-"""
-for i in listOfPossibleUserNames :
-    print(i, end= ' ')
-"""
-"""
-for i in listOfPossibleMobileNumbers :
-    print(i, end=' ')
-"""
-"""
-dobList = [day, month, year]
-for i in dobList :
-    print(i, end=' ')
-"""
+#appending both combinations of mobile number into list of digits , from which we select a pair of digit for our password
+for i in range(2) :
+    listOfPossibleDigits.append(listOfPossibleMobileNumbers[i])
 
-digits = [day, year, year[2:], mobNumber[0:5], mobNumber[len(mobNumber)-3:]]
-for i in digits :
-    print(i)
+#Now add date of birth & birth year & last 2 digits of birth year to listOfPossibleDigits[]
+listOfPossibleDigits.append(day)
+listOfPossibleDigits.append(year)
+listOfPossibleDigits.append(year[len(year)-2:])
 
+#Now also append month of birth to both upper & lowercase list
+listOfLowerCaseUserNames.append(month.lower())
+listOfUpperCaseUserNames.append(month.upper())
+
+#Selecting 4 different special characters & store into possible list of special characters
 specialCharacters = string.punctuation
-finalPasswordListGenerator = [listOfPossibleUserNames, digits, specialCharacters]
-finalStrongPassword = ''
+for i in range(4) :
+    listOfPossibleSpecialCharacters.append(random.choice(specialCharacters))
 
-listOfUppercasePassword = [username.split(' ')[0][:2], username.split(' ')[0][]]
+#We create this list to randomly select any 1 i.e. username's or username initials list
+lower_random_list = [listOfLowerCaseUserNames, listOfLowerCaseUserNameInitials]
+#We create this list to randomly select any 1 i.e. uppercase username's or uppercase username initials list
+upper_random_list = [listOfUpperCaseUserNames, listOfUpperCaseUserNameInitials]
 
-for i in range(3) :
-    ls = finalPasswordListGenerator[random.randint(0, len(finalPasswordListGenerator)-1)]
-    finalStrongPassword += str(ls[random.randint(0, len(ls)-1)])
+#Now randomly select anyone, we now have 1 random uppercase list & 1 random lowercase list
+lower_selected_list = random.choice(lower_random_list)
+upper_selected_list = random.choice(upper_random_list)
 
-print("final password is : " + finalStrongPassword)
+#This is final list, which contains 1 uppercase list, 1 lowercase list, 2 list of special characters & 1 list of possible digits to make our password strong
+finalSelectedList = [lower_selected_list, upper_selected_list, listOfPossibleDigits, listOfPossibleSpecialCharacters, listOfPossibleSpecialCharacters]
+#now shuffle the list, so that password pattern is not specific to elements position in list i.e. first lowercase in passwordm then uppercase in password
+random.shuffle(finalSelectedList)
+
+#Now loop through the shuffled list. Here we get 1st list from shuffled list
+#Then, we randomly select anyone combination from that list & concatenate it to our password which is in string form
+for i in finalSelectedList :
+    num = random.randint(0, len(i)-1)
+    finalStrongPassword += i[num]
+
+#finally again check if password length is less than 10
+#If password length < 10, we keep on choosing randomly between uppercase, lowercase, digits & specialCharacters from finalSelectedList[]
+#We keep on shuffling & keep on choosing random elements
+while(len(finalStrongPassword)<10) :
+    random.shuffle(finalSelectedList)
+    anyList = random.choice(finalSelectedList)
+    anyNum = random.choice(anyList)
+    finalStrongPassword += anyNum
+
+print('Strong Password generated is : ' + finalStrongPassword)
