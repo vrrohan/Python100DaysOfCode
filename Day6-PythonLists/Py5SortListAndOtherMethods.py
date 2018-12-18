1b0b4dce402ec5661a6c97b6303db7c0e8afd542
Rohan Yadav <rohan.yadav@gmail.com>
2018-12-18 19:30:40 +0530

    Sort a list in both aesc and desc order, also reverse a list, sum a list

--- Day6-PythonLists/Py5SortListAndOtherMethods.py
+++ Day6-PythonLists/Py5SortListAndOtherMethods.py
@@ -18,3 +18,21 @@ list_of_marks = [78, 89, 65, 92, 70, 56, 45, 82, 70]
 print('marks before erase : ', list_of_marks)                       #marks before erase :  [78, 89, 65, 92, 70, 56, 45, 82, 70]
 list_of_marks.reverse()
 print('marks after reverse : ', list_of_marks)                      #marks after reverse :  [70, 82, 45, 56, 70, 92, 65, 89, 78]
+
+#Get sum of list having integers or floats only
+#Python provide an inbuilt function sum() which sums up the numbers in the list.
+#sum(listName)
+listOfNums = [1, 2, 3, 4, 5]
+print('sum of numbers is : ', sum(listOfNums))                           #sum of numbers is :  15
+#sum(listName, start) - returns sum of all numbers + start
+print('sum of numbers is : ', sum(listOfNums, 20))                       #sum of numbers is :  35
+
+#TypeError : This error is raised in the case when there is anything other then numbers in the list.
+"""
+listOfNames = ['abc', 'xyz', 'mno']
+print('sum of names : ', sum(listOfNames))
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+"""
+
+lis = [2.3, 4.5, 6.7]
+print(sum(lis))                 #13.5
