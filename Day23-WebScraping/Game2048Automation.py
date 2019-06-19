aba45b1edaea8e95d3def21cedbce5c7dca29f13
vrrohan <rohaan.vr@gmail.com>
2019-06-19 23:33:52 +0530

    [unfinished]2048 game bot that plays game to create largest tile within given moves

--- Day23-WebScraping/Game2048Automation.py
+++ Day23-WebScraping/Game2048Automation.py
@@ -1,10 +1,11 @@
 import pyautogui, requests, random
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
-pyautogui.PAUSE = 0.8
+pyautogui.PAUSE = 0.5
 pyautogui.FAILSAFE = True
-
-g2048_game_weblink = 'http://play2048.co/'
+GAME_2048_WEBLINK = 'http://play2048.co/'
+GOOGLE_CHROME_DRIVER_WINDOWS_PATH = 'D:\\Python\\python for web\\chromedriver_win32\\chromedriver.exe'
+GAME_2048_WEBPAGE_LOAD_TIMEOUT = 45
 
 def getResponseFrom2048Link(gameLinkResponse) :
     try :
@@ -15,32 +16,31 @@ def getResponseFrom2048Link(gameLinkResponse) :
 
 def displayWebsiteInChrome(browser) :
     try :
-        browser.get(g2048_game_weblink)
-        browser.set_page_load_timeout(45)
+        browser.get(GAME_2048_WEBLINK)
+        browser.set_page_load_timeout(GAME_2048_WEBPAGE_LOAD_TIMEOUT)
     except Exception as e :
         print('Website timeout, unable to load website in 45 secs', e)
 
-gameLinkResponse = requests.get(g2048_game_weblink)
+#Main Program execution starts from here...
+gameLinkResponse = requests.get(GAME_2048_WEBLINK)
 getResponseFrom2048Link(gameLinkResponse)
 botMoves = int(input('select bot moves between 75 to 300 : '))
-#Get the object for handling chrome browser
-browser = webdriver.Chrome(executable_path=r"D:\python\chromedriver_win32\chromedriver.exe")
-displayWebsiteInChrome(browser)
-#get to start the game
-pyautogui.click(200, 200)
-pyautogui.press('down')
-pyautogui.press('left')
-pyautogui.press('left')
-pyautogui.press('right')
-keyPresses = ['left', 'right', 'up', 'down']
-soup = bs(gameLinkResponse.text, 'html.parser')
-print(soup)
-for i in range(botMoves) :
-    key = keyPresses[random.randint(0, len(keyPresses)-1)]
-    pyautogui.press(key)
-    res = requests.get(g2048_game_weblink)
-    ss = bs(res.text, 'html.parser')
-    s = ss.findAll(class_='tile-inner')
-    print(browser.find_element_by_xpath('/html/body/div[3]/div[4]/div[3]/div[5]/div'))
-    print('inner tile : ', s)
-    print('key pressed : ' + key)
+if botMoves >= 75 and botMoves <=300 :
+    #Get the object for handling chrome browser
+    browser = webdriver.Chrome(executable_path=GOOGLE_CHROME_DRIVER_WINDOWS_PATH)
+    displayWebsiteInChrome(browser)
+    #get to start the game
+    pyautogui.click(200, 200)
+    keyPresses = ['left', 'down']
+    leftDownPressed = 1
+    rightPressed = 1
+    for i in range(botMoves) :
+        key = keyPresses[random.randint(0, len(keyPresses)-1)]
+        if i%15==0 and i>=15 :
+            pyautogui.press('right')
+            print('right pressed', i)
+        else :
+            pyautogui.press(key)
+            print(str(key) + ' key pressed...' +  str(i))
+else :
+    print('bot moves limit out of given range. execution stops.')
